5b9fcc9217e74c9134e9fbeb747700864d860e0d
Julien Mellon <mellon.julien@gmail.com>
2022-09-13 08:22:27 -0400

    changed schema

--- models/poohquotes.js
+++ models/poohquotes.js
@@ -1,104 +1,105 @@
 const poohQuotes = {
-    1: {
+    count: 20,
+    data: [{
         quote: "You can't stay in your corner of the Forest waiting for others to come to you. You have to go to them sometimes.",
         author: "winnie-the-pooh",
         tags: ['pooh', 'forest']
     },
-    2: {
+    {
         quote: "Promise me you'll always remember: You're braver than you believe, stronger than you seem, and smarter than you think.",
         author: "winnie-the-pooh",
         tags: ['pooh', 'promise']
     },
-    3: {
+    {
         quote: "You never can tell with bees.",
         author: "winnie-the-pooh",
         tags: ['pooh', 'bees']
     },
-    4: {
+    {
         quote: "A hug is always the right size.",
         author: "winnie-the-pooh",
         tags: ['pooh', 'friendship', 'love']
     },
-    5: {
+    {
         quote: "After all, one can't complain. I have my friends.",
         author: "Eeyore",
         tags: ['eeyore', 'friendship']
     },
-    6: {
+    {
         quote: "The things that make me different are the things that make me, me.",
         author: "Piglet",
         tags: ['piglet', 'self']
     },
-    7: {
+    {
         quote: "Sometimes the smallest things take up the most room in your heart.",
         author: "winnie-the-pooh",
         tags: ['pooh', 'love', 'heart']
     },
-    8: {
+    {
         quote: "If you live to be a hundred, I hope I live to be a hundred minus one day, so that I never have to live a day without you.",
         author: "winnie-the-pooh",
         tags: ['pooh', 'bees', 'friendship']
     },
-    9: {
+    {
         quote: "A little consideration, a little thought for others, makes all the difference.",
         author: "Eeyore",
         tags: ['eeyore', 'consideration', 'friendship']
     },
-    10: {
+     {
         quote: "I'm so rumbly in my tumbly.",
         author: "winnie-the-pooh",
         tags: ['pooh', 'hunger']
     },
-    11: {
+    {
         quote: "I always get to where I'm going by walking away from where I have been.",
         author: "winnie-the-pooh",
         tags: ['pooh', 'goals']
     },
-    12: {
+    {
         quote: "If the string breaks, then we try another piece of string.",
         author: "owl",
         tags: ['owl', 'goals', 'perseverance']
     },
-    13: {
+    {
         quote: "If you live to be a hundred, I hope I live to be a hundred minus one day, so that I never have to live a day without you.",
         author: "winnie-the-pooh",
         tags: ['pooh', 'bees', 'friendship']
     },
-    14: {
+    {
         quote: "It isn't much good having anything exciting, if you can't share it with somebody.",
         author: "winnie-the-pooh",
         tags: ['pooh', 'goals', 'friendship']
     },
-    15: {
+    {
         quote: "It is more fun to talk with someone who doesn't use long, difficult words but rather short, easy words like, 'What about lunch?'",
         author: "winnie-the-pooh",
         tags: ['pooh', 'bees', 'friendship']
     },
-    16: {
+    {
         quote: "Rivers know this: there is no hurry. We shall get there some day.",
         author: "winnie-the-pooh",
         tags: ['pooh', 'bees', 'friendship']
     },
-    17: {
+    {
         quote: "Weeds are flowers, too, once you get to know them.",
         author: "winnie-the-pooh",
         tags: ['pooh']
     },
-    18: {
+    {
         quote: "A day without a friend is like a pot without a single drop of honey left inside.",
         author: "winnie-the-pooh",
         tags: ['pooh', 'friendship']
     },
-    19: {
+    {
         quote: "If the person you are talking to doesn’t appear to be listening, be patient. It may simply be that he has a small piece of fluff in his ear.",
         author: "winnie-the-pooh",
         tags: ['pooh']
     },
-    20: {
+    {
         quote: "The things that make me different are the things that make me.",
         author: "winnie-the-pooh",
         tags: ['pooh']
-    }
+    }]
 }
 
 
