ee8e1f461401dab7219188895a6f123f73f389d5
Julien Mellon <mellon.julien@gmail.com>
2022-09-18 07:35:19 -0400

    reverted routes

--- routes/main.js
+++ routes/main.js
@@ -4,9 +4,8 @@ const poohController = require("../controllers/pooh");
 
 //Main Routes - simplified for now
 router.get("/", poohController.getIndex);
-router.get("/api", poohController.getAll);
+router.get("/api", poohController.getPooh);
 router.get("/random", poohController.getRandom);
-router.get('/api/tigger/:tigger', poohController.piglet);
-router.get("/api/:query", poohController.getPooh);
+router.get('/api/:piglet', poohController.piglet)
 
 module.exports = router;
